a15ad55fed19f5b1dc53ebe7e6b71ee5da0d10be
khaoula <khaoulajabour4@gmail.com>
2021-12-17 00:38:51 -0500

    replace the hardcoded value with PORT

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const {animals} = require('./data/animals');
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -58,6 +59,6 @@ app.get('/api/animals', (req, res) => {
 
 
 
-app.listen(3001, ()=>{
+app.listen(PORT, ()=>{
     console.log('API server now on port 3001!');
 });
